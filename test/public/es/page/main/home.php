eede9b7115f06a491cd8e6a84859b3b7d5bdfb37
JeanPiffaut <piffaut.jean@gmail.com>
2020-10-17 15:15:00 -0300

    Update

--- test/public/es/page/main/home.php
+++ test/public/es/page/main/home.php
@@ -48,12 +48,96 @@ global $CONFIG;
                 </div>
             </div>
         </div>
-        <div class="card p-3 bg-color-2 mt-5">
+    </section>
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+                <p class="text-left h1 color-4">
+                    <i class="fas fa-beer"></i>
+                </p>
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid amet beatae cum dolor doloribus fugit.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <p class="text-left h1 color-4">
+                    <i class="fas fa-beer"></i>
+                </p>
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid amet beatae cum dolor doloribus fugit.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <p class="text-left h1 color-4">
+                    <i class="fas fa-beer"></i>
+                </p>
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid amet beatae cum dolor doloribus fugit.</p>
+            </div>
+        </div>
+    </section>
+    <section class="container py-5">
+        <div class="card p-3 bg-color-2">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid amet beatae cum dolor doloribus fugit hic in, labore laboriosam libero mollitia odio, quas qui quibusdam quidem ratione sapiente, temporibus.
         </div>
     </section>
     <section class="container py-5">
-
+        <div class="px-md-5">
+            <div class="px-md-5">
+                <div class="row">
+                    <div class="col-12 col-md-6 mb-4 mb-md-0">
+                        <div class="card px-3 py-5 lift">
+                            <p class="mt-4">Lorem</p>
+                            <h2 class="mb-0">Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <div class="row">
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Lorem ipsum
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Dolor sit amet
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Consectetur adipisicing
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Aliquid dignissimos
+                                </div>
+                            </div>
+                            <button class="btn btn-primary btn-block btn-lg mb-4 mt-4">Dolor sit</button>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="px-3 py-5">
+                            <p class="mt-4">Lorem</p>
+                            <h2 class="mb-0">Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <div class="row">
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Lorem ipsum
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Dolor sit amet
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Consectetur adipisicing
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <i class="fas fa-check"></i>
+                                    Aliquid dignissimos
+                                </div>
+                            </div>
+                            <button class="btn btn-primary btn-block btn-lg mb-4 mt-4">Dolor sit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <section style="height: 500px;">
 
